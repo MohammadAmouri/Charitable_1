f84504be39a2c422a3c20f54aa99c7da99eed47b
Your Name <you@example.com>
2018-09-16 20:16:08 +0300

    first draft

---
+++ app/src/main/java/com/developers/developers/AlgeriaActivity.java
@@ -0,0 +1,22 @@
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// Mohammad Alamouri - Charitable Team
+// Algeria Activity
+
+
+package com.developers.developers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AlgeriaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_algeria);
+    }
+
+
+}
